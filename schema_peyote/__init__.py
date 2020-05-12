f2747f3a082020f89898961ad3b64cbbf14f8468
Abe <abegong@gmail.com>
2020-05-12 10:23:44 -0700

    Works. Untested

---
+++ schema_peyote/__init__.py
@@ -0,0 +1,1 @@
+from .flattener import flatten_json_to_dataframes, summarize_json_tree, derive_features_from_summary
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='schema_peyote',
+    version='0.1',
+    description='utility scripts for unpacking JSON to tabular form',
+    url='http://github.com/superconductive/schema_peyote',
+    author='Abe Gong',
+    license='Apache 2.0',
+    packages=['schema_peyote'],
+    zip_safe=False
+)
